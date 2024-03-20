6dbe7dc62fd74c62a040a52880833e320620c624
Jesus Rocha <jesus.rocha@ibm.com>
2024-03-20 12:11:55 -0700

    add github workflows

---
+++ scripts/build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#
+# Copyright 2017-2024 Elyra Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -e
+
+WORKING_DIR="$PWD"
+SCRIPT_DIR=$(dirname "$0")
+
+echo "cd $SCRIPT_DIR"
+cd $SCRIPT_DIR
+# install require modules
+echo "npm install --legacy-peer-deps"
+npm install --legacy-peer-deps
+echo "npm run build"
+npm run build
+
+echo "cd $WORKING_DIR"
+cd $WORKING_DIR
